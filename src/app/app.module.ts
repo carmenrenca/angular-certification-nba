8697bbc594ead0da051355c47f4f2ed726440059
Rendon <crendon@deloitte.es>
2023-04-28 20:00:31 +0200

    change name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule  } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { RouterModule } from '@angular/router';
 import { SearchComponent } from './pages/search/search.component';
-import { CardTeamComponent } from './pages/search/cards-item/card-team.component';
+import { CardTeamComponent } from './pages/search/card-team/card-team.component';
 import { ResultsComponent } from './pages/results/results.component';
 import { SharedModule } from './shared/shared.module';
 
